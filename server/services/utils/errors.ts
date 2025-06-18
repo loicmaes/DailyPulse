4df0119f2ebb9e996c9f2da20ee74b9520cf84b4
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-06-18 23:30:52 +0200

    :rocket: Add errors utils service

---
+++ server/services/utils/errors.ts
@@ -0,0 +1,16 @@
+import type { DailyException } from "~/types/utils/exceptions";
+import type { HttpEvent, StatusCode } from "~/types/utils/http";
+
+export function hanleException(event: HttpEvent, exception: DailyException) {
+  setOutput(event, exception.code, exception.message);
+
+  return {
+    code: exception.code,
+    message: exception.message,
+  };
+}
+
+export function setOutput(event: HttpEvent, code: StatusCode, message: string) {
+  event.node.res.statusCode = code;
+  event.node.res.statusMessage = message;
+}
