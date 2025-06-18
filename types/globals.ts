ed07b23846a45bd2958a6c99c88657aee8ba79f6
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-06-18 17:41:20 +0200

    :rocket: Add prisma models related tabs

---
+++ types/globals.ts
@@ -0,0 +1,1 @@
+export type TNull<T> = T | null;
---
+++ types/user.ts
@@ -0,0 +1,12 @@
+export interface IBackUser {
+  id: string;
+  email: string;
+  username: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+export type IUser = Omit<IBackUser, "password">;
+
+export type IUserCreate = Omit<IBackUser, "id" | "createdAt" | "updatedAt">;
+export type IUserUpdate = Partial<IUserCreate>;
