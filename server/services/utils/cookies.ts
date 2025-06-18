0609b9a380fd39e9ef686c24526fb363905c9c55
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-06-18 23:42:31 +0200

    :rocket: Add authetication token cookies service

---
+++ server/services/utils/cookies.ts
@@ -0,0 +1,34 @@
+import type { CookieSerializeOptions } from "cookie-es";
+import type { HttpEvent } from "~/types/utils/http";
+
+export const TOKEN_COOKIE = "auth-token";
+export const USER_COOKIE = "user-id";
+export const COOKIES_OPTIONS: CookieSerializeOptions = {
+  path: "/",
+  httpOnly: true,
+  sameSite: "strict",
+  secure: true,
+};
+
+export function getAuthCookies(event: HttpEvent): {
+  token?: string;
+  userId?: string;
+} {
+  const token = getCookie(event, TOKEN_COOKIE);
+  const userId = getCookie(event, USER_COOKIE);
+
+  return {
+    token,
+    userId,
+  };
+}
+
+export function setAuthCookies(event: HttpEvent, token: string, userId: string) {
+  setCookie(event, TOKEN_COOKIE, token, COOKIES_OPTIONS);
+  setCookie(event, USER_COOKIE, userId, COOKIES_OPTIONS);
+}
+
+export function clearAuthCookies(event: HttpEvent) {
+  deleteCookie(event, TOKEN_COOKIE);
+  deleteCookie(event, USER_COOKIE);
+}
