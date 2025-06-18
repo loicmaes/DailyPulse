59be8c7e36c554f30c853ca4eb1d62195f486685
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-06-19 00:35:04 +0200

    :rocket: Add user logging type

--- types/user.ts
+++ types/user.ts
@@ -10,3 +10,5 @@ export type IUser = Omit<IBackUser, "password">;
 
 export type IUserCreate = Omit<IBackUser, "id" | "createdAt" | "updatedAt">;
 export type IUserUpdate = Partial<IUserCreate>;
+
+export type IUserLogging = Pick<IBackUser, "email" | "password">;
