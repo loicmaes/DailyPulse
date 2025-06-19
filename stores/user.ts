728c954e4f5b9bc4a4624fbc81bdd8e9018a85dd
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-06-19 13:48:02 +0200

    :rocket: Setup user store

---
+++ stores/user.ts
@@ -0,0 +1,82 @@
+import { toast } from "vue-sonner";
+import type { TNull } from "~/types/utils/globals";
+import type { IUser, IUserCreate, IUserLogin } from "~/types/user";
+
+interface UserState {
+  user: TNull<IUser>;
+  loading: boolean;
+}
+
+export const useUserStore = defineStore("user", {
+  state: (): UserState => ({
+    user: null,
+    loading: false,
+  }),
+  getters: {
+    translate: () => useNuxtApp().$i18n.t,
+    isLoggedIn: (state): boolean => !!state.user,
+  },
+  actions: {
+    async getMe() {
+      try {
+        const { data } = await useFetch<IUser>("/api/users/me");
+        if (!data.value) return;
+        this.user = data.value;
+      }
+      catch (e) {
+        console.error(e);
+      }
+    },
+    async login(body: IUserLogin): Promise<boolean> {
+      this.loading = true;
+
+      try {
+        const user = await $fetch<IUser>("/api/auth/login", {
+          method: "POST",
+          body,
+        });
+
+        this.user = user;
+        toast.success(this.translate("toasts.auth.login.success", { username: user.username }));
+        return true;
+      }
+      catch {
+        toast.error(this.translate("toasts.auth.login.error"));
+        return false;
+      }
+      finally {
+        this.loading = false;
+      }
+    },
+    async register(body: IUserCreate): Promise<boolean> {
+      this.loading = true;
+
+      try {
+        const user = await $fetch<IUser>("/api/auth/register", {
+          method: "POST",
+          body,
+        });
+
+        this.user = user;
+        toast.success(this.translate("toasts.auth.register.success", { username: user.username }));
+        return true;
+      }
+      catch {
+        toast.error(this.translate("toasts.auth.register.error"));
+        return false;
+      }
+      finally {
+        this.loading = false;
+      }
+    },
+    async logout() {
+      toast.promise($fetch<IUser>("/api/auth/logout", {
+        method: "DELETE",
+      }), {
+        loading: this.translate("toasts.auth.logout.loading"),
+        success: this.translate("toasts.auth.logout.success"),
+        error: this.translate("toasts.auth.logout.error"),
+      });
+    },
+  },
+});
