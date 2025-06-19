1582d877e209e3a2b8a255dc4ecf2e09a2068cfb
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-06-19 22:31:26 +0200

    :rocket: Setup basic default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,12 @@
 </script>
 
 <template>
-  <div data-layout="default">
-    <NuxtPage />
+  <div
+    data-layout="default"
+    class="px-4 flex flex-col items-center"
+  >
+    <div class="w-[min(1080px,100%)] py-4">
+      <NuxtPage />
+    </div>
   </div>
 </template>
