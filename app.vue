8c512a9a956edee79e0a16b9148fc0bf4237393c
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-06-19 13:48:23 +0200

    :rocket: Add sonner in the app

--- app.vue
+++ app.vue
@@ -1,7 +1,28 @@
+<script setup lang="ts">
+import "vue-sonner/style.css";
+import { useMediaQuery } from "@vueuse/core";
+
+const colorMode = useColorMode();
+const theme = computed((): "dark" | "light" => {
+  if (colorMode.preference === "system")
+    return useMediaQuery("(prefers-color-scheme: dark)").value ? "dark" : "light";
+
+  return colorMode.preference as "dark" | "light";
+});
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
     <NuxtLoadingIndicator />
+
+    <ClientOnly>
+      <Toaster
+        position="top-center"
+        :theme="theme"
+        rich-colors
+      />
+    </ClientOnly>
     <NuxtLayout />
   </div>
 </template>
