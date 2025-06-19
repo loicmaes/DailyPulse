04c8a1e4c03a2b7a80fbdf6b3c70cf625c3b2046
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-06-19 16:21:36 +0200

    :rocket: Add list query types

--- types/utils/globals.ts
+++ types/utils/globals.ts
@@ -1,1 +1,16 @@
 export type TNull<T> = T | null;
+
+export interface ListResult<T> {
+  data: T[];
+  meta: {
+    total: number;
+    count: number;
+    page: number;
+    perPage: number;
+  };
+}
+export interface ListQuery {
+  page?: number;
+  perPage?: number;
+  search?: string;
+}
