787280e46d3768e7c199c8701a0e97e9a03e0d2d
loicmaes <production@maesloic.fr>
2025-06-20 22:49:55 +0200

    :rocket: Add finances navigation modules and dialog

---
+++ components/shared/finances/dialogs/TransactionDialog.vue
@@ -0,0 +1,123 @@
+<script setup lang="ts">
+import { useForm } from "vee-validate";
+import { LoaderCircle } from "lucide-vue-next";
+import { toTypedSchema } from "@vee-validate/zod";
+import { z } from "zod";
+import type { ETransactionType, ITransaction } from "~/types/finances/transactions";
+
+const open = defineModel<boolean>("open");
+watch(open, (val) => {
+  if (!val) return;
+  resetForm();
+});
+
+const props = defineProps<{
+  transaction?: ITransaction;
+  type: ETransactionType;
+}>();
+
+const store = useFinancesStore();
+const { loading } = storeToRefs(store);
+const editMode = computed(() => !!props.transaction);
+
+const { handleSubmit, resetForm } = useForm({
+  validationSchema: toTypedSchema(z.object({
+    label: z.string(),
+    amount: z.number().min(0),
+    note: z.string().optional(),
+  })),
+  initialValues: {
+    label: props.transaction?.label,
+    amount: props.transaction?.amount,
+    note: props.transaction?.note,
+  },
+});
+const submit = handleSubmit(async (values) => {
+  const create = async (): Promise<boolean> => store.addTransaction({
+    ...values,
+    type: props.type,
+    date: new Date(),
+  });
+  const save = async (): Promise<boolean> => true;
+
+  const state = editMode.value ? await save() : await create();
+  if (!state) return;
+  open.value = false;
+});
+</script>
+
+<template>
+  <Dialog v-model:open="open">
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>{{ $t("app.finances.dashboard.dialogs.new-transaction.title", type === "expense" ? 2 : 1) }}</DialogTitle>
+      </DialogHeader>
+
+      <form
+        class="grid gap-4"
+        @submit="submit"
+      >
+        <FormField
+          v-slot="{ componentField }"
+          name="label"
+        >
+          <FormItem>
+            <FormLabel>{{ $t("app.finances.dashboard.dialogs.new-transaction.fields.label") }}</FormLabel>
+            <FormControl v-bind="componentField">
+              <Input :disabled="loading" />
+            </FormControl>
+          </FormItem>
+        </FormField>
+        <FormField
+          v-slot="{ componentField }"
+          name="amount"
+        >
+          <FormItem>
+            <FormLabel>{{ $t("app.finances.dashboard.dialogs.new-transaction.fields.amount") }}</FormLabel>
+            <FormControl v-bind="componentField">
+              <Input
+                type="number"
+                inputmode="numeric"
+                placeholder="ex. 41.32"
+                :disabled="loading"
+              />
+            </FormControl>
+          </FormItem>
+        </FormField>
+        <FormField
+          v-slot="{ componentField }"
+          name="note"
+        >
+          <FormItem>
+            <FormLabel>{{ $t("app.finances.dashboard.dialogs.new-transaction.fields.note") }} <span class="text-sm text-muted-foreground">{{ $t("labels.optional") }}</span></FormLabel>
+            <FormControl v-bind="componentField">
+              <Textarea :disabled="loading" />
+            </FormControl>
+          </FormItem>
+        </FormField>
+
+        <DialogFooter>
+          <Button
+            type="button"
+            variant="secondary"
+            :disabled="loading"
+            @click="open = false"
+          >
+            {{ $t("btn.cancel") }}
+          </Button>
+          <Button
+            type="submit"
+            :disabled="loading"
+            @click="open = false"
+          >
+            <LoaderCircle
+              v-if="loading"
+              class="animate-spin"
+            />
+            {{ $t("app.finances.dashboard.dialogs.new-transaction.action") }}
+          </Button>
+        </DialogFooter>
+      </form>
+    </DialogContent>
+  </Dialog>
+</template>
---
+++ pages/app/finances/index.vue
@@ -0,0 +1,108 @@
+<script setup lang="ts">
+import { BanknoteArrowDown, BanknoteArrowUp, Plus, Search } from "lucide-vue-next";
+import TransactionsTable from "~/components/shared/finances/table/TransactionsTable.vue";
+import TransactionDialog from "~/components/shared/finances/dialogs/TransactionDialog.vue";
+import type { ETransactionType } from "~/types/finances/transactions";
+
+const store = useFinancesStore();
+store.loadTransactions();
+
+const dialogOpen = ref<boolean>(false);
+const newTransactionType = ref<ETransactionType>();
+
+const openDialog = (type: ETransactionType) => {
+  newTransactionType.value = type;
+  dialogOpen.value = true;
+};
+</script>
+
+<template>
+  <main
+    data-page="app.finances.dashboard"
+    class="grid gap-4"
+  >
+    <header class="py-4 flex flex-col items-start gap-6 md:flex-row md:items-center md:justify-between md:gap-0">
+      <div class="grid">
+        <h1 class="text-2xl font-bold">
+          {{ $t("app.finances.dashboard.title") }}
+        </h1>
+        <p class="text-muted-foreground">
+          {{ $t("app.finances.dashboard.caption") }}
+        </p>
+      </div>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger as-child>
+          <Button>
+            <Plus />
+            {{ $t("app.finances.dashboard.action.label") }}
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem @click="openDialog('income')">
+            <BanknoteArrowUp />
+            {{ $t("app.finances.dashboard.action.options.new-income") }}
+          </DropdownMenuItem>
+          <DropdownMenuItem @click="openDialog('expense')">
+            <BanknoteArrowDown />
+            {{ $t("app.finances.dashboard.action.options.new-expense") }}
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <TransactionDialog
+        v-model:open="dialogOpen"
+        :type="newTransactionType"
+      />
+    </header>
+
+    <section class="grid md:grid-cols-3 gap-4">
+      <Card>
+        <CardHeader class="flex">
+          <CardTitle class="text-sm">
+            Abonnements mensuels
+          </CardTitle>
+        </CardHeader>
+      </Card>
+      <Card>
+        <CardHeader class="flex">
+          <CardTitle class="text-sm">
+            Abonnements annuels
+          </CardTitle>
+        </CardHeader>
+      </Card>
+      <Card>
+        <CardHeader class="flex">
+          <CardTitle class="text-sm">
+            Économies réalisées
+          </CardTitle>
+        </CardHeader>
+      </Card>
+    </section>
+    <section v-if="false">
+      <Card>
+        <CardHeader class="flex">
+          <CardTitle class="text-sm">
+            Activité
+          </CardTitle>
+        </CardHeader>
+      </Card>
+    </section>
+    <section>
+      <Card class="p-0 gap-0 grid">
+        <header class="flex items-center gap-2 p-2 border-b">
+          <div class="relative flex-1">
+            <Input
+              class="pl-8"
+              :placeholder="$t('labels.search')"
+              disabled
+            />
+            <Search class="absolute top-2.5 left-2.5 size-4 text-muted-foreground" />
+          </div>
+        </header>
+
+        <TransactionsTable />
+      </Card>
+    </section>
+  </main>
+</template>
