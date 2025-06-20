5c4b5f339745fcec480cd9f1ba9710a9b6e6cbdf
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-06-20 12:07:10 +0200

    :bug: Add period date filtering

--- server/services/moods.ts
+++ server/services/moods.ts
@@ -42,12 +42,17 @@ export async function removeEntry(event: HttpEvent) {
 export async function recoverTodayMoodBoard(event: HttpEvent) {
   const user = event.context.user;
   const query = getQuery<Omit<ListQuery, "period"> & { period: string }>(event);
+
   if (!query?.period) return handleException(event, new BadRequestException("Period query info isn't provided!"));
+
+  const period = JSON.parse(query.period as string);
+  period.start = new Date(period.start);
+  period.end = new Date(period.end);
 
   try {
     const moodBoard = await moodEntries.getUserEntries(user.id, {
       ...query,
-      period: JSON.parse(query.period as string),
+      period,
     });
 
     if (moodBoard.meta.count === 0) setOutput(event, StatusCode.NO_CONTENT, "Your mood board is actually empty!");
